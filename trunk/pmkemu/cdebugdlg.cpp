a23689efe1c53f839cea74a47bff2e719d6c4608
felix.lazarev@gmail.com <felix.lazarev@gmail.com@393d58b9-6be9-b1a8-32cf-cb8545bc60c8>
2012-01-05 04:25:55 +0000

    fixed micro disassembler and debug window

--- trunk/pmkemu/cdebugdlg.cpp
+++ trunk/pmkemu/cdebugdlg.cpp
@@ -31,12 +31,12 @@ cDebugDlg::cDebugDlg(QWidget *parent) :
 
     rg.setRgb(200,200,200);
 
-    for(i=0;i<3;i++)
-        for(j=0;j<42;j++)
+    for(i=0;i<9;i++)
+        for(j=0;j<14;j++)
         {
-            items[i*42+j]=new QTableWidgetItem(QString().sprintf("%X",(i*42+j)%16));
-            items[i*42+j]->setFlags(Qt::ItemIsEnabled);
-            ui->regTable->setItem(i,j,items[i*42+j]);
+            items[i*14+j]=new QTableWidgetItem(QString().sprintf("%X",(i*14+j)%16));
+            items[i*14+j]->setFlags(Qt::ItemIsEnabled);
+            ui->regTable->setItem(i,j,items[i*14+j]);
             //ui->regTable->item(i,j)->setBackgroundColor(QColor().setRgb(128,128,128));
 
             rt=ui->regTable->item(i,j)->background();
@@ -66,13 +66,19 @@ void cDebugDlg::onItemChange(QTableWidgetItem *itm)
     switch(itm->row())
     {
         case 0:
-            dlg->setReg(QString().sprintf("M[%d]=",itm->column()),i&0xf);
-            break;
         case 1:
-            dlg->setReg(QString().sprintf("R[%d]=",itm->column()),i&0xf);
-            break;
         case 2:
-            dlg->setReg(QString().sprintf("ST[%d]=",itm->column()),i&0xf);
+            dlg->setReg(QString().sprintf("M%d[%d]=",itm->row()+1,itm->column()),i&0xf);
+            break;
+        case 3:
+        case 4:
+        case 5:
+            dlg->setReg(QString().sprintf("R%d[%d]=",itm->row()+1,itm->column()),i&0xf);
+            break;
+        case 6:
+        case 7:
+        case 8:
+            dlg->setReg(QString().sprintf("ST%d[%d]=",itm->row()+1,itm->column()),i&0xf);
             break;
     }
 
@@ -87,20 +93,29 @@ void cDebugDlg::onItemChange(QTableWidgetItem *itm)
 
 void cDebugDlg::setI(unsigned int i)
 {
-    int j;
+    int r,c,j;
     QColor rg;
 #if 1
     rg.setRgb(200,200,200);
-    for(j=0;j<42;j++)
+    for(j=0;j<14;j++)
     {
         ui->regTable->item(0,j)->setBackgroundColor(rg);
         ui->regTable->item(1,j)->setBackgroundColor(rg);
         ui->regTable->item(2,j)->setBackgroundColor(rg);
+        ui->regTable->item(3,j)->setBackgroundColor(rg);
+        ui->regTable->item(4,j)->setBackgroundColor(rg);
+        ui->regTable->item(5,j)->setBackgroundColor(rg);
+        ui->regTable->item(6,j)->setBackgroundColor(rg);
+        ui->regTable->item(7,j)->setBackgroundColor(rg);
+        ui->regTable->item(8,j)->setBackgroundColor(rg);
+
     }
+    r=i%3;
+    c=i/3;
     rg.setRgb(128,255,128);
-    ui->regTable->item(0,i)->setBackgroundColor(rg);
-    ui->regTable->item(1,i)->setBackgroundColor(rg);
-    ui->regTable->item(2,i)->setBackgroundColor(rg);
+    ui->regTable->item(0+r,c)->setBackgroundColor(rg);
+    ui->regTable->item(3+r,c)->setBackgroundColor(rg);
+    ui->regTable->item(6+r,c)->setBackgroundColor(rg);
 #endif
 }
 
@@ -149,7 +164,9 @@ void cDebugDlg::setREGS(bool rm[],bool rr[],bool rst[],unsigned int rlen, unsign
     unsigned char curst;
     unsigned int bitcnt;
     unsigned int ptr;
+    int r,c;
 
+#if 1
     ptr=0;
     while(curu!=0)
     {
@@ -184,9 +201,11 @@ void cDebugDlg::setREGS(bool rm[],bool rr[],bool rst[],unsigned int rlen, unsign
         curu++;
         if(curu>=4)
         {
-            ui->regTable->item(0,curi)->setText(QString().sprintf("%1.1X",curm));
-            ui->regTable->item(1,curi)->setText(QString().sprintf("%1.1X",curr));
-            ui->regTable->item(2,curi)->setText(QString().sprintf("%1.1X",curst));
+            r=curi%3;
+            c=curi/3;
+            ui->regTable->item(r,c)->setText(QString().sprintf("%1.1X",curm));
+            ui->regTable->item(3+r,c)->setText(QString().sprintf("%1.1X",curr));
+            ui->regTable->item(6+r,c)->setText(QString().sprintf("%1.1X",curst));
             curu=0;
             curi++;
             if(curi>=(rlen/4))
@@ -197,6 +216,7 @@ void cDebugDlg::setREGS(bool rm[],bool rr[],bool rst[],unsigned int rlen, unsign
             ptr=0;
     }
 
+#endif
 
 
 
