f43f9aadf469f723f8ca0731b3e0f025c3adc135
svofski <svofski@393d58b9-6be9-b1a8-32cf-cb8545bc60c8>
2012-09-01 15:28:02 +0000

    windows automated build script

---
+++ buildscripts/deploy.bat
@@ -0,0 +1,47 @@
+set DEPLOYDIR=pmkemu
+set QT4SDK=C:\QtSDK
+set QT4SDKM=c:/QtSDK
+set MINGW=c:\MinGW
+
+PATH=%PATH%;c:\bin
+
+s:
+mkdir emu145-build-dir
+subst m: emu145-build-dir
+
+set BDISK=m:
+set BDIR=emu145-build
+set MINGWDIR=%BDISK%/%BDIR%/pmkemu
+
+%BDISK%
+cd \
+del /q/s %BDIR%
+svn checkout http://emu145.googlecode.com/svn/trunk/ %BDIR%
+cd %BDIR%\pmkemu
+
+
+echo cd %MINGWDIR% >build.sh
+echo %QT4SDKM%/Desktop/Qt/4.8.0/mingw/bin/qmake.exe >>build.sh
+echo make -f Makefile.Release >>build.sh
+
+
+%MINGW%\msys\1.0\bin\sh --login -i %MINGWDIR%/build.sh
+
+del /q /s %DEPLOYDIR%
+mkdir %DEPLOYDIR%
+copy /b release\pmkemu.exe %DEPLOYDIR%
+copy /b "%MINGW%\bin\libstdc++-6.dll" %DEPLOYDIR%
+copy /b "%QT4SDK%\Desktop\Qt\4.8.0\mingw\bin\libgcc_s_dw2-1.dll" %DEPLOYDIR%
+copy /b "%QT4SDK%\Desktop\Qt\4.8.0\mingw\bin\QtCore4.dll" %DEPLOYDIR%
+copy /b "%QT4SDK%\Desktop\Qt\4.8.0\mingw\bin\mingwm10.dll" %DEPLOYDIR%
+copy /b "%QT4SDK%\Desktop\Qt\4.8.0\mingw\bin\QtGui4.dll" %DEPLOYDIR%
+
+
+for /f "tokens=1,2,3,4 delims=/ " %%a in ("%date%") do set wday=%%a&set month=%%b&set day=%%c&set year=%%d
+for /f "tokens=1,2,3,4 delims=:. " %%a in ("%time%") do set t1=%%a&set t2=%%b&set t3=%%c&set t4=%%d
+
+set ZIPNAME=pmkemu_snapshot_%year%%month%%day%_%t1%%t2%.zip
+
+zip %ZIPNAME% -r %DEPLOYDIR%
+
+..\..\buildscripts\googlecode-upload.py -s "snapshot build" -p emu145 %ZIPNAME%
