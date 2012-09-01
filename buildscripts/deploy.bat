155d76a7e32b7aead1c3a045918cc3851938c4fb
svofski <svofski@393d58b9-6be9-b1a8-32cf-cb8545bc60c8>
2012-09-01 16:02:19 +0000

    build script update

--- buildscripts/deploy.bat
+++ buildscripts/deploy.bat
@@ -3,15 +3,20 @@ set QT4SDK=C:\QtSDK
 set QT4SDKM=c:/QtSDK
 set MINGW=c:\MinGW
 
-PATH=%PATH%;c:\bin
 
-s:
-mkdir emu145-build-dir
-subst m: emu145-build-dir
-
+set REALBUILDDISK=s:
 set BDISK=m:
 set BDIR=emu145-build
 set MINGWDIR=%BDISK%/%BDIR%/pmkemu
+
+
+PATH=%PATH%;c:\bin
+
+pushd
+
+%REALBUILDDISK%
+mkdir emu145-build-dir
+subst %BDISK% emu145-build-dir
 
 %BDISK%
 cd \
@@ -40,8 +45,12 @@ copy /b "%QT4SDK%\Desktop\Qt\4.8.0\mingw\bin\QtGui4.dll" %DEPLOYDIR%
 for /f "tokens=1,2,3,4 delims=/ " %%a in ("%date%") do set wday=%%a&set month=%%b&set day=%%c&set year=%%d
 for /f "tokens=1,2,3,4 delims=:. " %%a in ("%time%") do set t1=%%a&set t2=%%b&set t3=%%c&set t4=%%d
 
-set ZIPNAME=pmkemu_snapshot_%year%%month%%day%_%t1%%t2%.zip
+set ZIPNAME=pmkemu_snapshot_win32_%year%%month%%day%_%t1%%t2%.zip
 
 zip %ZIPNAME% -r %DEPLOYDIR%
 
-..\..\buildscripts\googlecode-upload.py -s "snapshot build" -p emu145 %ZIPNAME%
+..\buildscripts\googlecode_upload.py -s "win32 snapshot build" -p emu145 %ZIPNAME%
+
+
+popd
+subst /d %BDISK%
